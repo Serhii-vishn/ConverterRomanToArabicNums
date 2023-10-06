a206b1a62f5807aecdf53bd735ec7225c05126c5
Serhii <66913762+Serhii-vishn@users.noreply.github.com>
2023-10-06 09:55:18 +0300

    Transferred the dictionary to the class

--- ConverterRomanToArabicNums/Program.cs
+++ ConverterRomanToArabicNums/Program.cs
@@ -8,25 +8,28 @@ namespace ConverterRomanToArabicNums
 {
     internal class Program
     {
+        public static Dictionary<char, int> romanianNums = new Dictionary<char, int>()
+        {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'M', 1000}
+        };
+
         static void Main(string[] args)
-        {           
-            Console.Write("Converter, enter romanian num: "); string romanianStr = Console.ReadLine();
+        {
+            Console.WriteLine("\t\tConverter Roman to arabian numbers");
+            Console.WriteLine("\t--------------------------------------------------");
+            Console.Write("Enter romanian num: "); string romanianStr = Console.ReadLine();
  
             Console.WriteLine("Arabian num: " + ConverterRomToArabian(romanianStr.ToUpper()));
             Console.ReadLine();
         }
 
         public static int ConverterRomToArabian (string romanianStr)
-        {
-            var romanianNums = new Dictionary<char, int>();
-            romanianNums.Add('I', 1);
-            romanianNums.Add('V', 5);
-            romanianNums.Add('X', 10);
-            romanianNums.Add('L', 50);
-            romanianNums.Add('C', 100);
-            romanianNums.Add('D', 500);
-            romanianNums.Add('M', 1000);
-
+        {       
             int lastNum = romanianStr.Length - 1;
             int result = romanianNums[romanianStr[lastNum]];
             int arabian;
