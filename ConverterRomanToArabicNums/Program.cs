882fb718e9bca586f642efa5a414ae3fadf6e172
Serhii <66913762+Serhii-vishn@users.noreply.github.com>
2023-10-06 10:36:56 +0300

    Add input validation

--- ConverterRomanToArabicNums/Program.cs
+++ ConverterRomanToArabicNums/Program.cs
@@ -18,32 +18,56 @@ namespace ConverterRomanToArabicNums
             {'M', 1000}
         };
 
+        public static bool ValidateInput(string input)
+        {
+            foreach (char c in input)
+            {
+                if (!romanianNums.ContainsKey(c))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("\t\tConverter Roman to arabian numbers");
-            Console.WriteLine("\t--------------------------------------------------");
-            Console.Write("Enter romanian num: "); string romanianStr = Console.ReadLine();
- 
-            Console.WriteLine("Arabian num: " + ConverterRomToArabian(romanianStr.ToUpper()));
-            Console.ReadLine();
+            while(true)
+            {
+                Console.WriteLine("\t\tConverter Roman to arabian numbers");
+                Console.WriteLine("\t--------------------------------------------------");
+                Console.Write("Enter romanian num: "); string romanianStr = Console.ReadLine();
+
+                if (ValidateInput(romanianStr))
+                    Console.WriteLine("Arabian num: " + ConverterRomToArabian(romanianStr.ToUpper()));
+                else
+                    Console.WriteLine("Error! invalid symbol. Try Again");
+
+                Console.Write("\nExit - *, complete - any: "); var k = Console.ReadKey();
+                if (k.KeyChar == '*')
+                    break;
+            }
         }
 
         public static int ConverterRomToArabian (string romanianStr)
         {       
             int lastNum = romanianStr.Length - 1;
+            
+            int nxtArabian = romanianNums[romanianStr[lastNum]];
             int result = romanianNums[romanianStr[lastNum]];
-            int arabian;
+
             for (int i = lastNum - 1; i >= 0; i--)
             {
-                arabian = romanianNums[romanianStr[i]];
-                if(arabian < romanianNums[romanianStr[i+1]])
+                int currArabian = romanianNums[romanianStr[i]];
+                if(currArabian < nxtArabian)
                 {
-                    result -= arabian;
+                    result -= currArabian;
                 }
                 else
                 {
-                    result += arabian;
+                    result += currArabian;
                 }
+                nxtArabian = currArabian;
             }           
             return result;
         }
