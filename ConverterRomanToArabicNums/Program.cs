9977c7a85bfc7fbab8a35e6b76620490f8c6e132
Serhii <66913762+Serhii-vishn@users.noreply.github.com>
2023-10-16 11:55:20 +0300

    added functionality and a dictionary for converting Arabic to Roman numbers

--- ConverterRomanToArabicNums/Program.cs
+++ ConverterRomanToArabicNums/Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConverterRomanToArabicNums
 {
@@ -78,7 +75,6 @@ namespace ConverterRomanToArabicNums
                 }
                 Console.ReadLine();
             }
-
         }
 
         public static int ConverterRomToArabian (string romanianStr)
@@ -107,7 +103,31 @@ namespace ConverterRomanToArabicNums
         public static string ConverterArabToRomanian(int number)
         {
             string result = string.Empty;
+            Dictionary<string, int> arabicNums = new Dictionary<string, int>()
+            {
+                {"I", 1},
+                {"IV", 4},
+                {"V", 5},
+                {"IX", 9},
+                {"X", 10},
+                {"XL", 40},
+                {"L", 50},
+                {"XC", 90},
+                {"C", 100},
+                {"CD", 400},
+                {"D", 500},
+                {"CM", 900},
+                {"M", 1000}
+            };
 
+            foreach (var paitNums in arabicNums.OrderByDescending(x => x.Value))
+            {
+                while (number >= paitNums.Value)
+                {
+                    result += paitNums.Key;
+                    number -= paitNums.Value;
+                }
+            }
             return result;
         }
     }
